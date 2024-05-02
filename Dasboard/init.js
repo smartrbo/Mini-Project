89a4602ea8b9c65636e643ac5669540183d8f731
Kartik-Doye <140334885+Kartik-Doye@users.noreply.github.com>
2024-05-03 00:43:57 +0530

    Frontend Completed

---
+++ Dasboard/init.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCsb7ntUbzGWSRrvLB8xyWhqTTS9H_jzPg",
+  authDomain: "authenticationfirebase-a0db9.firebaseapp.com",
+  projectId: "authenticationfirebase-a0db9",
+  storageBucket: "authenticationfirebase-a0db9.appspot.com",
+  messagingSenderId: "694165744693",
+  appId: "1:694165744693:web:3ce3e800ca1372d718157c",
+  measurementId: "G-CS504426G9"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
